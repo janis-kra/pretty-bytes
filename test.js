d82bb821054c428e738c2e63a0001befee02a2e5
Janis Krasemann <janis.krasemann@enpit.de>
2016-09-01 22:01:25 +0200

    rewrite tests for use with the IEC byte units

--- test.js
+++ test.js
@@ -17,17 +17,18 @@ test('converts bytes to human readable strings', t => {
 	t.is(m(0.7), '0.7 B');
 	t.is(m(10), '10 B');
 	t.is(m(10.1), '10.1 B');
-	t.is(m(999), '999 B');
-	t.is(m(1001), '1 kB');
-	t.is(m(1001), '1 kB');
-	t.is(m(1e16), '10 PB');
-	t.is(m(1e30), '1000000 YB');
+	t.is(m(1020), '1020 B');
+	t.is(m(1025), '1 kiB');
+	t.is(m(1130), '1.1 kiB');
+	t.is(m(1e16), '8.88 PiB'); // 8.882 PiB
+	t.is(m(1e19), '8.67 EiB'); // 8.674 EiB
+	t.is(m(1.209e30), '1000000 YiB');
 });
 
 test('supports negative number', t => {
 	t.is(m(-0.4), '-0.4 B');
 	t.is(m(-0.7), '-0.7 B');
 	t.is(m(-10.1), '-10.1 B');
-	t.is(m(-999), '-999 B');
-	t.is(m(-1001), '-1 kB');
+	t.is(m(-1020), '-1020 B');
+	t.is(m(-1025), '-1 kiB');
 });
